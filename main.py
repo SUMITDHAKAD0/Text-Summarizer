ceac69b0a9853edf8989972ccf1f96110f3fb19e
SUMITDHAKAD0 <sumit.dhakad9644@gmail.com>
2023-06-13 17:36:00 +0530

    pipeline & component

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from src.logging import logger
+from src.pipeline.data_ingestion01 import DataIngestionTrainingPipeline
+from src.pipeline.data_validation02 import DataValidationTrainingPipeline
 
-logger.info('Welcome to Text Summarizer')
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/utils/common.py
+++ src/utils/common.py
@@ -7,8 +7,6 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
-
-
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
@@ -47,8 +45,6 @@ def create_directories(path_to_directories: list, verbose=True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
-
-
 
 @ensure_annotations
 def get_size(path: Path) -> str:
